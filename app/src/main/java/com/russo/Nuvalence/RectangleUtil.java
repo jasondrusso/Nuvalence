90b3a67342770834ed6c85d64dee7c64eed591e4
Jason Russo <jdrusso@juno.com>
2022-10-24 12:15:22 -0400

    Add Rectangle utility functions.

--- app/src/main/java/com/russo/Nuvalence/RectangleUtil.java
+++ app/src/main/java/com/russo/Nuvalence/RectangleUtil.java
@@ -4,12 +4,30 @@
  */
 package com.russo.Nuvalence;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 /**
  *
  * @author jason
  */
 public class RectangleUtil {
-    public boolean isIntersecting(Rectangle r1, Rectangle r2) {
+    
+    public enum Axis {
+        X, Y
+    }
+    
+    /**
+     * Test whether rectangles intersect.
+     * 
+     * @param r1
+     * @param r2
+     * 
+     * @return 
+     * 
+     */
+    public static boolean isIntersecting(Rectangle r1, Rectangle r2) {
         return r1.getTop().isCrossed(r2.getLeft())
                 || r1.getTop().isCrossed(r2.getRight())
                 || r1.getBottom().isCrossed(r2.getLeft())
@@ -20,11 +38,74 @@ public class RectangleUtil {
                 || r1.getRight().isCrossed(r2.getBottom());
     }
     
-    public boolean isContained(Rectangle r1, Rectangle r2) {
+    /**
+     * Test whether either rectangle is contained within the other
+     * 
+     * @param r1
+     * @param r2
+     * 
+     * @return 
+     */
+    public static boolean isContained(Rectangle r1, Rectangle r2) {
+        return !isIntersecting(r1, r2) && (isWithin(r1, r2) || isWithin(r2, r1));
+    }
+    
+    public static boolean isAdjacent(Rectangle r1, Rectangle r2) {
+        return !isContained(r1, r2) && isEdgeAdjacent(r1, r2);
+    }
+    
+    private static boolean isEdgeAdjacent(Rectangle r1, Rectangle r2) {
+        List<Line> rl1 = List.of(r1.getTop(), r1.getRight(), r1.getBottom(), r1.getLeft());
+        List<Line> rl2 = List.of(r2.getTop(), r2.getRight(), r2.getBottom(), r2.getLeft());
+        
+        for (Line l1 : rl1) {
+            for (Line l2 : rl2) {
+                if (l1.isAdjacent(l2))
+                    return true;
+            }
+        }
+        
         return false;
     }
     
-    public boolean isAdjacent(Rectangle r1, Rectangle r2) {
-        return false;
-    }    
+    private static boolean isWithin(Rectangle outer, Rectangle inner) {
+        List<Double> outerX = getVerticies(outer, Axis.X);
+        List<Double> outerY = getVerticies(outer, Axis.Y);
+        List<Double> innerX = getVerticies(inner, Axis.X);
+        List<Double> innerY = getVerticies(inner, Axis.Y);
+        
+        double minOuterX = Collections.min(outerX);
+        double maxOuterX = Collections.max(outerX);
+        
+        double minOuterY = Collections.min(outerY);
+        double maxOuterY = Collections.max(outerY);
+        
+        double minInnerX = Collections.min(innerX);
+        double maxInnerX = Collections.max(innerX);
+        
+        double minInnerY = Collections.min(innerY);
+        double maxInnerY = Collections.max(innerY);
+        
+        return minOuterX < minInnerX && maxOuterX > maxInnerX &&
+                minOuterY < minInnerY && maxOuterY > maxInnerY;
+    }
+    
+    private static List<Double> getVerticies(Rectangle rect, Axis axis) {
+        switch (axis) {
+            case X:
+                return Arrays.asList(
+                        rect.getTopLeft().x,
+                        rect.getTopRight().x,
+                        rect.getBottomLeft().x,
+                        rect.getBottomRight().x);
+            case Y:
+                return Arrays.asList(
+                        rect.getTopLeft().y,
+                        rect.getTopRight().y,
+                        rect.getBottomLeft().y,
+                        rect.getBottomRight().y);
+        }
+        
+        return null;
+    }
 }
--- app/src/test/java/com/russo/Nuvalence/RectangleUtilTest.java
+++ app/src/test/java/com/russo/Nuvalence/RectangleUtilTest.java
@@ -4,6 +4,7 @@
  */
 package com.russo.Nuvalence;
 
+import java.io.InvalidObjectException;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -13,7 +14,32 @@ import static org.junit.jupiter.api.Assertions.*;
  */
 public class RectangleUtilTest {
     
-    public RectangleUtilTest() {
+    @Test
+    public void testIntersecting() throws InvalidObjectException {
+        Point topLeft1 = new Point(2.0, 9.0);
+        Point bottomRight1 = new Point(9.0, 2.0);
+        
+        Point topLeft2 = new Point(5.0, 11.0);
+        Point bottomRight2 = new Point(11.0, 5.0);
+        
+        Rectangle rect1 = new Rectangle(topLeft1, bottomRight1);
+        Rectangle rect2 = new Rectangle(topLeft2, bottomRight2);
+        
+        assertTrue(RectangleUtil.isIntersecting(rect1, rect2));
+    }
+    
+    @Test
+    public void testNotIntersecting() throws InvalidObjectException {
+        Point topLeft1 = new Point(2.0, 9.0);
+        Point bottomRight1 = new Point(9.0, 2.0);
+        
+        Point topLeft2 = new Point(10.0, 12.0);
+        Point bottomRight2 = new Point(15.0, 6.0);
+        
+        Rectangle rect1 = new Rectangle(topLeft1, bottomRight1);
+        Rectangle rect2 = new Rectangle(topLeft2, bottomRight2);
+        
+        assertFalse(RectangleUtil.isIntersecting(rect1, rect2));
     }
     
 }
